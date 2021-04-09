9bf9d62a7042a964413e23dfc2a82d3bc5a2dc98
h00ngen <laurynasgudav@gmail.com>
2021-04-09 13:04:18 +0300

    Add files via upload

---
+++ RSAEncryption/RSAEncryption/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace RSAEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtencrypt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtdecrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtplain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtencrypt
+            // 
+            this.txtencrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtencrypt.ForeColor = System.Drawing.Color.Red;
+            this.txtencrypt.Location = new System.Drawing.Point(12, 326);
+            this.txtencrypt.Multiline = true;
+            this.txtencrypt.Name = "txtencrypt";
+            this.txtencrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtencrypt.Size = new System.Drawing.Size(564, 68);
+            this.txtencrypt.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(564, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Dešifruoti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtdecrypt
+            // 
+            this.txtdecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdecrypt.Location = new System.Drawing.Point(14, 499);
+            this.txtdecrypt.Multiline = true;
+            this.txtdecrypt.Name = "txtdecrypt";
+            this.txtdecrypt.Size = new System.Drawing.Size(564, 68);
+            this.txtdecrypt.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(564, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Šifruoti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtplain
+            // 
+            this.txtplain.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtplain.Location = new System.Drawing.Point(12, 92);
+            this.txtplain.Multiline = true;
+            this.txtplain.Name = "txtplain";
+            this.txtplain.Size = new System.Drawing.Size(564, 68);
+            this.txtplain.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Užsifruotas tekstas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(186, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 36);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "RSA Algoritmas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Paprastas tekstas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(179, 477);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Paprastas tekstas po dešifravimo";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(564, 44);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Skaityti nuo failo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(61, 278);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(137, 13);
+            this.fileNameLabel.TabIndex = 15;
+            this.fileNameLabel.Text = "* čia matysite failo lokaciją *\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "File path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 579);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtencrypt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtdecrypt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtplain);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSA Algoritmas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtencrypt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtdecrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtplain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RSAEncryption/RSAEncryption/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #region-----Sifravimas ir desifravimas-----
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
+        #endregion
+
+        #region--Kintamieji
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+        #endregion
+
+        #region-- Funkcijos Implementacija
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            txtencrypt.Text = Convert.ToBase64String(encryptedtext, Base64FormattingOptions.InsertLineBreaks);
+            var localLink = "C:/Users/Laurynas/Desktop/RSAEncryption/RSAEncryption/encrypted.txt";
+            File.WriteAllText(localLink, Convert.ToBase64String(encryptedtext, Base64FormattingOptions.InsertLineBreaks));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string asd = File.ReadAllText(fileNameLabel.Text);
+            byte[] src = Convert.FromBase64String(asd);
+
+            byte[] decryptedtex = Decryption(src, RSA.ExportParameters(true), false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+        }
+        #endregion
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileNameLabel.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
